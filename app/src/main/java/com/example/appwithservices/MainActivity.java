f5ed057a814cd26e99388e5ac3e24e55982238d6
sherruh <sherruh@gmail.com>
2019-04-07 18:25:45 +0600

    add notification

--- app/src/main/java/com/example/appwithservices/MainActivity.java
+++ app/src/main/java/com/example/appwithservices/MainActivity.java
@@ -4,15 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
     Intent intentService;
+    EditText et;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et=findViewById(R.id.et);
+        progressBar=findViewById(R.id.progress_bar);
+        progressBar.setVisibility(View.INVISIBLE);
     }
 
     public void onClickStart(View view) {
@@ -28,9 +35,13 @@ public class MainActivity extends AppCompatActivity {
     public void onClickStartIntent(View view) {
         intentService =new Intent(this,MyIntentService.class);
         startService(intentService);
+        progressBar.setVisibility(View.VISIBLE);
     }
 
     public void onClickStopIntent(View view) {
-        if(intentService!=null) stopService(intentService);
+        if(intentService!=null) {
+            stopService(intentService);
+            progressBar.setVisibility(View.GONE);
+        }
     }
 }
--- app/src/main/java/com/example/appwithservices/MyIntentService.java
+++ app/src/main/java/com/example/appwithservices/MyIntentService.java
@@ -1,24 +1,40 @@
 package com.example.appwithservices;
 
+import android.annotation.TargetApi;
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationChannelGroup;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 public class MyIntentService extends IntentService {
 
     boolean isStopped;
+    NotificationManager manager;
 
     public MyIntentService() {
         super("");
     }
+
+
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
         startTask();
     }
 
+
+
+
     private void startTask(){
+        showNotification();
         for(int i=0;i<10;i++){
             if (isStopped) return;
             Log.d("MyApp","Service task on "+i+" step");
@@ -30,10 +46,34 @@ public class MyIntentService extends IntentService {
         }
     }
 
+    private void showNotification() {
+        if(Build.VERSION.SDK_INT==Build.VERSION_CODES.O){
+        NotificationChannel channel=new NotificationChannel("my_channel",
+                "new_message", NotificationManager.IMPORTANCE_HIGH);
+        manager=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.createNotificationChannel(channel);
+        NotificationCompat.Builder notification=new NotificationCompat.Builder(getBaseContext(),"my_channel");
+        notification.setContentTitle(getString(R.string.app_name));
+        notification.setContentText("works");
+        notification.setSmallIcon(R.mipmap.ic_launcher);
+        startForeground(1,notification.build());
+        }else{
+            NotificationCompat.Builder mBuilder =   new NotificationCompat.Builder(this)
+                    .setSmallIcon(R.mipmap.ic_launcher) // notification icon
+                    .setContentTitle(getString(R.string.app_name)) // title for notification
+                    .setContentText("works") // message for notification
+                    .setAutoCancel(true);
+            manager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            manager.notify(0, mBuilder.build());
+        }
+    }
+
     @Override
     public void onDestroy() {
         super.onDestroy();
         Log.d("MyApp","MyIntentService stopped");
         isStopped=true;
+        manager.cancel(0);
     }
 }
